2bd0503223e653530c6cef0d5b1e4c7181e73c9e
epp <edward2a@gmail.com>
2020-05-01 06:16:15 +0100

    add in-use AMIs output (non duplicate)

--- aws/fetch_non-asg_instances.py
+++ aws/fetch_non-asg_instances.py
@@ -31,6 +31,10 @@ def load_args():
         '--no-print-static', action='store_false', default=True,
         dest='print_static', help='Do not print static instances')
 
+    p.add_argument(
+        '--print-ami', action='store_true', default=False,
+        dest='print_ami', help='Print the list of AMIs in use.')
+
     return p.parse_args()
 
 
@@ -54,8 +58,9 @@ def get_as_groups(client):
 
 
 def get_ec2_non_asg_instances(client, as_instances):
-    """Get all EC2 instances by batch, return ones not in as_instances."""
+    """Get all EC2 instances by batch, return not in as_instances and AMIs."""
     non_asg_i = []
+    amis = set()
 
     ec2_describe_p = client.get_paginator('describe_instances')
 
@@ -68,29 +73,37 @@ def get_ec2_non_asg_instances(client, as_instances):
     for page in ec2_describe_p.paginate():
         for reservation in page['Reservations']:
             for instance in reservation['Instances']:
+
+                amis.update([instance['ImageId']])
+        
                 if instance['InstanceId'] not in as_instances:
                     non_asg_i.append(instance['InstanceId'])
 
-    return non_asg_i
+    return non_asg_i, amis
 
 
-def process_region(region=DEFAULT_REGION, asg_i=True, sta_i=True):
+def process_region(region, args):
     """Execute main function, region scope."""
     asg = boto3.client('autoscaling', region_name=region)
     ec2 = boto3.client('ec2', region_name=region)
 
     as_groups, as_instances = get_as_groups(asg)
-    non_asg_i = get_ec2_non_asg_instances(ec2, as_instances)
+    non_asg_i, amis = get_ec2_non_asg_instances(ec2, as_instances)
 
-    if asg_i:
+    if args.print_asg:
         print('\n\t==== AutoScaling Instances - %s ====\n' % region)
         for i in as_instances:
             print(i)
 
-    if sta_i:
+    if args.print_static:
         print('\n\t==== Static Instances  - %s ====\n' % region)
         for i in non_asg_i:
             print(i)
+
+    if args.print_ami:
+        print('\n\t==== AMIs in use - %s ====\n' % region)
+        for a in amis:
+            print(a)
 
 
 if __name__ == '__main__':
@@ -99,5 +112,5 @@ if __name__ == '__main__':
 
     if args.config is None:
         for r in args.region:
-            process_region(
-                region=r, asg_i=args.print_asg, sta_i=args.print_static)
+            process_region(r, args)
+
